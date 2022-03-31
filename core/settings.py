78edd473acb4495039d997616ff82c9d817d434b
Adarsh01111 <102159256+Adarsh01111@users.noreply.github.com>
2022-03-31 22:49:46 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0q%_w$%ysprytbnz%%9_(onevk$8-151sa@#)$ri19hk+ie36a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Flase
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://hdpbyadarsh.herokuapp.com/']
 
